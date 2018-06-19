5929c0115707c7f500fd321ab481b980813bf0b0
Bobby <karma0@gmail.com>
2018-06-19 15:27:04 -0600

    Cleanup of initial scaffolding

--- astronaut/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Bobby Larson'
-__email__ = 'bobby@robot.studio'
-__version__ = '0.1.0'
-
-from astronaut import Astronaut
--- astronaut/astronaut.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Astronaut(object):
-
-	def __init__(self):
-		pass
