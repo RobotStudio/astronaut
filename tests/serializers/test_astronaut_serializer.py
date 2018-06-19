0d14fec2ac24a135cf906ed55b5bb081bd7bea0c
Bobby <karma0@gmail.com>
2018-06-19 15:30:15 -0600

    Added serializer basic test

---
+++ tests/serializers/test_astronaut_serializer.py
@@ -0,0 +1,24 @@
+import json
+
+from rentomatic.serializers import astronaut_serializer as srs
+from rentomatic.domain import models
+
+
+def test_serialize_domain_astronaut():
+    room = models.Astronaut('f853578c-fc0f-4e65-81b8-566c5dffa35a',
+                              size=200,
+                              price=10,
+                              longitude='-0.09998975',
+                              latitude='51.75436293')
+
+    expected_json = """
+{
+"code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+"size": 200,
+"price": 10,
+"longitude": -0.09998975,
+"latitude": 51.75436293
+}
+"""
+
+    assert json.loads(json.dumps(room, cls=srs.AstronautEncoder)) == json.loads(expected_json)
